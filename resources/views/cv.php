d23302d8172519f22e0a055e87c3b6d4ae2d2d27
renaldi17 <renaldipasya2004@gmail.com>
2021-03-04 18:54:01 +0700

    first commit

---
+++ resources/views/cv.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>CURRICULUM VITAE</title>
+
+    </head>
+    <body>
+    <h1 align=center>CURRICULUM VITAE</h1>
+    <!--INFORMASI DATA DIRI-->
+        <hr>
+        <b>PERSONAL INFORMATION</b>
+        <hr>
+        <table style="width:50%">
+            <!--FOTO-->
+            <img src="fotoku.jpg" align="right" width="104" height="142">
+        <tr>
+        <th></th>
+        <th></th>
+        </tr>
+        <tr>
+            <td>Nama Lengkap</td>
+            <td>: Renaldi Pasya Darmansyah </td>
+        </tr>
+        <tr>
+            <td>Tempat Tanggal Lahir</td>
+            <td>: Bandung, 15 Maret 2004 </td>  
+        </tr>
+            <td>Alamat</td>
+            <td>: Perumahan Taman Asri </td>
+        <tr>
+            <td>Status</td>
+            <td>: Siswa</td>
+        </tr>
+            <tr>
+            <td>NO HP</td>
+            <td>: 089610001144</td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td>: renaldipasya2004@gmail</td>
+        </tr>
+        </table>
+    <!--RIWAYAT PENDIDIKAN FORMAL-->  
+            <hr>
+            <b>PENDIDIKAN FORMAL</b>
+            <hr>
+                <table style="width:80%">
+            <tr>
+            <th></th>
+            <th></th>
+            </tr>
+            <tr>
+                <td>2019-2022</td>
+                <td>SMK Negeri 2 Sukabumi </td>
+            </tr>
+            <tr>
+                <td>2016-2019</td>
+                <td>SMPIT AL-HUDA </td>  
+            </tr>
+                <td>2010-2016</td>
+                <td>SDN Cisarua</td>
+            <tr>
+                <td>2008-2010</td>
+                <td>TK Al-Azhar 7</td>
+            </tr>
+            </table>
+    <!-- RIWAYAT ORGANISASI-->
+                            <hr>
+                            <b>ORGANISASI</b><hr>
+                            <table style="width:180%">
+                        <tr>
+                        <th></th>
+                        <th></th>
+                        </tr>
+                        <tr>
+                            <td>2019</td>
+                            <td>Anggota PMR SMKN 2 Sukabumi</td>
+                        </tr>
+                        </table>
+    <!--KEAHLIAN YANG DIMILIKI-->                  
+                            <hr>
+                            <b>KEAHLIAN</b><hr>
+                        Mengoperasikan Komputer : Microsoft Office<br>
+                        Desain Grafis: CorelDraw <br>
+    <a href="indeks.html">KEMBALI KE INDEKS</a><br>
+    </body>
+</html>
